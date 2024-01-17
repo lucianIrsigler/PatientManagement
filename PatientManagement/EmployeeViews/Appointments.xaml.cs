b0d083fd4d9437574023c931ecf442e10700542e
CopyandPaste-png <l.irsigler64@gmail.com>
2024-01-17 13:34:15 +0200

    Added frontend designs for employee view

--- PatientManagement/EmployeeViews/Appointments.xaml.cs
+++ PatientManagement/EmployeeViews/Appointments.xaml.cs
@@ -23,6 +23,15 @@ namespace PatientManagement.EmployeeViews
         public Appointments()
         {
             InitializeComponent();
+
+            Loaded += (s, e) =>
+            {
+                double[] dataX = { 1, 2, 3, 4, 5 };
+                double[] dataY = { 1, 4, 9, 16, 25 };
+                WpfPlot1.Plot.Add.Scatter(dataX, dataY);
+                WpfPlot1.Refresh();
+            };
+
         }
 
         private void Create_Consultation(object sender, RoutedEventArgs e)
